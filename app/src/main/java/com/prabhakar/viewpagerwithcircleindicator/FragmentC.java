ab9c5a01f1c75f922a1d7914281d08afc789eacb
Prabhakar <prabhakarkr802@gmail.com>
2021-08-11 03:43:55 +0530

    ViewPager with circle indicator(Assignment MASAI School)

---
+++ app/src/main/java/com/prabhakar/viewpagerwithcircleindicator/FragmentC.java
@@ -0,0 +1,30 @@
+package com.prabhakar.viewpagerwithcircleindicator;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentC extends Fragment {
+    @Override
+    public void onCreate(@Nullable  Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_c, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull  View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
